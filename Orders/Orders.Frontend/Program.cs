d40112fd3ac38512d0966fd6f933baec29e41b8b
Marcelo Pírez <marcelopirez@hotmail.com>
2025-02-18 19:38:43 -0300

    Add ForntEnd CountriesIndex
    Hacemos la primer vista en el frontend con blazor

--- Orders/Orders.Frontend/Program.cs
+++ Orders/Orders.Frontend/Program.cs
@@ -9,7 +9,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 // Al cambiar esta línea que viene por defecto lo que le estamos diciendo la URL puesta es la que va a proveer los servicios de backend 
 // builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7053/") });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7254/") });
 builder.Services.AddScoped<IRepository, Repository>();
 
 await builder.Build().RunAsync();
