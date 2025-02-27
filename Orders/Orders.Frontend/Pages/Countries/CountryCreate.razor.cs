fe0e261807d79845e62e1b90270d4b0c45a9e9d5
marcelouy <marcelopirez@hotmail.com>
2025-02-27 18:20:14 -0300

    Tutorial 72 - Parte 08 - Crear un nuevo país
    https://www.youtube.com/watch?v=mKpkMDl85Ns&list=PLuEZQoW9bRnRBThyGs208ZMrCYBRTvIg2&index=9
    
    0:00 Crear el countryform
    16:50 Crear el formulario
    25:05 Crear el CountryCreate
    42:30 Probar el formulario

---
+++ Orders/Orders.Frontend/Pages/Countries/CountryCreate.razor.cs
@@ -0,0 +1,44 @@
+﻿using CurrieTechnologies.Razor.SweetAlert2;
+using Microsoft.AspNetCore.Components;
+using Orders.Frontend.Repositories;
+using Orders.Shared.Entities;
+
+namespace Orders.Frontend.Pages.Countries
+{
+    public partial class CountryCreate
+    {
+        private Country country = new();
+        private CountryForm? countryForm;
+
+        [Inject] private IRepository repository { get; set; } = null!;
+        [Inject] private SweetAlertService SweetAlertService { get; set; } = null!;
+        [Inject] private NavigationManager navigationManager { get; set; } = null!;
+
+        private async Task CreateAsync()
+        {
+            var responseHttp = await repository.PostAsync("api/countries", country);
+            if (responseHttp.Error)
+            {
+                var message = await responseHttp.GetErrorMessageAsync();
+                await SweetAlertService.FireAsync("Error", message);
+                return;
+            }
+
+            Return();
+            var toast = SweetAlertService.Mixin(new SweetAlertOptions
+            {
+                Toast = true,
+                Position = SweetAlertPosition.Center,
+                ShowConfirmButton = true,
+                Timer = 3000,
+            });
+            await toast.FireAsync(icon: SweetAlertIcon.Success, message: "Registro creado con éxito.");
+        }
+
+        private void Return()
+        {
+            countryForm.FormPostedSuccessfully = true;
+            navigationManager.NavigateTo("/countries");
+        }
+    }
+}
