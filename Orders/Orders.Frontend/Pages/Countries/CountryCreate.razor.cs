778290f4d64863193e9c15cc9ea65a6fbcf9f741
marcelouy <marcelopirez@hotmail.com>
2025-02-28 18:09:40 -0300

    Tutorial 72 - Parte 10 - Borrar un país
    https://www.youtube.com/watch?v=NfDSDN5rRss&list=PLuEZQoW9bRnRBThyGs208ZMrCYBRTvIg2&index=10

--- Orders/Orders.Frontend/Pages/Countries/CountryCreate.razor.cs
+++ Orders/Orders.Frontend/Pages/Countries/CountryCreate.razor.cs
@@ -20,7 +20,7 @@ namespace Orders.Frontend.Pages.Countries
             if (responseHttp.Error)
             {
                 var message = await responseHttp.GetErrorMessageAsync();
-                await SweetAlertService.FireAsync("Error", message);
+                await SweetAlertService.FireAsync("Error", message, SweetAlertIcon.Error);
                 return;
             }
 
