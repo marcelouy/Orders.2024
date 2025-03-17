c6716493c760a242c2bbe97d6f34b64f44f0af20
marcelouy <marcelopirez@hotmail.com>
2025-03-17 13:24:32 -0300

    Parte 13 - CRUD Categorias
    https://www.youtube.com/watch?v=v13wBfaqYNI&list=PLuEZQoW9bRnRBThyGs208ZMrCYBRTvIg2&index=13
    
    0:00 Explicacion
    0:45 Crear las pages de categoria
    4:10 Crear el Index
    8:20 Modificar la barra de menu
    11:00 Probar
    13:00 Crear el CategoryCreate
    17:35 Probar el Create
    18:30 Category Edit
    24:45 Probar edit

--- Orders/Orders.Frontend/Pages/Categories/CategoriesIndex.razor.cs
+++ Orders/Orders.Frontend/Pages/Categories/CategoriesIndex.razor.cs
@@ -4,14 +4,14 @@ using Orders.Frontend.Repositories;
 using Orders.Shared.Entities;
 using System.Net;
 
-namespace Orders.Frontend.Pages.Countries
+namespace Orders.Frontend.Pages.Categories
 {
-    public partial class CountriesIndex
+    public partial class CategoriesIndex
     {
         [Inject] private IRepository Repository { get; set; } = null!;
         [Inject] private NavigationManager NavigationManager { get; set; } = null!;
         [Inject] private SweetAlertService SweetAlertService { get; set; } = null!;
-        public List<Country>? Countries { get; set; }
+        public List<Category>? Categories { get; set; }
 
         protected override async Task OnInitializedAsync()
         {
@@ -20,8 +20,8 @@ namespace Orders.Frontend.Pages.Countries
 
         private async Task LoadAsync()
         {
-            var responseHttp = await Repository.GetAsync<List<Country>>("api/countries");
-            Countries = responseHttp.Response;
+            var responseHttp = await Repository.GetAsync<List<Category>>("api/categories");
+            Categories = responseHttp.Response;
 
             if (responseHttp.Error)
             {
@@ -29,15 +29,15 @@ namespace Orders.Frontend.Pages.Countries
                 await SweetAlertService.FireAsync("Error", message, SweetAlertIcon.Error);
                 return;
             }
-            Countries = responseHttp.Response;
+            Categories = responseHttp.Response;
         }
 
-        private async Task DeleteAsync(Country country)
+        private async Task DeleteAsync(Category category)
         {
             var result = await SweetAlertService.FireAsync(new SweetAlertOptions
             {
                 Title = "Confirmación",
-                Text = $"¿Estas seguro de querer borrar el país : {country.Name}?",
+                Text = $"¿Estas seguro de querer borrar la categoría : {category.Name}?",
                 Icon = SweetAlertIcon.Question,
                 ShowCancelButton = true,
             });
@@ -48,12 +48,12 @@ namespace Orders.Frontend.Pages.Countries
                 return;
             }
 
-            var responseHttp = await Repository.DeleteAsync<Country>($"api/countries/{country.Id}");
+            var responseHttp = await Repository.DeleteAsync<Category>($"api/categories/{category.Id}");
             if (responseHttp.Error)
             {
                 if (responseHttp.HttpResponseMessage.StatusCode == HttpStatusCode.NotFound)
                 {
-                    NavigationManager.NavigateTo("/countries");
+                    NavigationManager.NavigateTo("/categories");
                 }
                 else
                 {
---
+++ Orders/Orders.Frontend/Pages/Categories/CategoryEdit.razor.cs
@@ -0,0 +1,71 @@
+﻿using CurrieTechnologies.Razor.SweetAlert2;
+using Microsoft.AspNetCore.Components;
+using Orders.Frontend.Pages.Countries;
+using Orders.Frontend.Repositories;
+using Orders.Shared.Entities;
+
+namespace Orders.Frontend.Pages.Categories
+{
+    public partial class CategoryEdit
+    {
+        private Category? category;
+        private CategoryForm? categoryForm;
+
+        [Inject] private NavigationManager NavigationManager { get; set; } = null!;
+        [Inject] private IRepository Repository { get; set; } = null!;
+        [Inject] private SweetAlertService SweetAlertService { get; set; } = null!;
+        [EditorRequired, Parameter] public int Id { get; set; }
+
+
+        protected async override Task OnParametersSetAsync()
+        {
+            var responseHttp = await Repository.GetAsync<Category>($"/api/categories/{Id}");
+
+            if (responseHttp.Error)
+            {
+                if (responseHttp.HttpResponseMessage.StatusCode == System.Net.HttpStatusCode.NotFound)
+                {
+                    NavigationManager.NavigateTo("categories");
+                }
+                else
+                {
+                    var messageError = await responseHttp.GetErrorMessageAsync();
+                    await SweetAlertService.FireAsync("Error", messageError, SweetAlertIcon.Error);
+                }
+            }
+            else
+            {
+                category = responseHttp.Response;
+            }
+        }
+
+
+        private async Task EditAsync()
+        {
+            var responseHttp = await Repository.PutAsync("api/categories", category);
+
+            if (responseHttp.Error)
+            {
+                var mensajeError = await responseHttp.GetErrorMessageAsync();
+                await SweetAlertService.FireAsync("Error", mensajeError, SweetAlertIcon.Error);
+                return;
+            }
+
+            Return();
+            var toast = SweetAlertService.Mixin(new SweetAlertOptions
+            {
+                Toast = true,
+                Position = SweetAlertPosition.BottomEnd,
+                ShowConfirmButton = true,
+                Timer = 3000
+            });
+            await toast.FireAsync(icon: SweetAlertIcon.Success, message: "Cambios guardados con éxito.");
+        }
+
+        private void Return()
+        {
+            categoryForm!.FormPostedSuccessfully = true;
+            NavigationManager.NavigateTo("categories");
+        }
+    }
+}
