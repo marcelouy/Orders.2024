d6cb9d15e51dfdbfb3dbf393ed6d33b51114fb75
marcelouy <marcelopirez@hotmail.com>
2025-03-24 16:18:14 -0300

    Tutorial 72 - Parte 15 - Utilizando un formulario genérico
    0:00 Repaso y explicacion
    2:00 Crear la Interfaces
    4:50 Crear el FormWithName
    12:50 Modificar el country create
    16:00 Probar el proyecto
    18:10 Borrar el CountryForm
    18:20 Modificar el category
    21:10 Probar Categorias
    
    https://www.youtube.com/watch?v=u9sE2aiuHHs&list=PLuEZQoW9bRnRBThyGs208ZMrCYBRTvIg2&index=16

---
+++ Orders/Orders.Shared/Entities/State.cs
@@ -0,0 +1,14 @@
+﻿using Orders.Shared.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orders.Shared.Entities
+{
+    public class State : IEntityWithName
+    {
+        public string Name { get; set; } = null!;
+    }
+}
