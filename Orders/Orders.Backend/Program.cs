ad19a1d02648844ff169a90b4a9efbb1575dc61b
marcelouy <marcelopirez@hotmail.com>
2025-03-24 13:24:41 -0300

    Parte 19 - Alimentador de base de datos
    0:00 Solucion a iconos con dos paths
    3:50 Crear el SeedDb
    7:40 Metodo CountriesAsync
    11:05 Edicion en bloque
    12:10 Inyectar el seed
    16:35 Probar
    
    https://www.youtube.com/watch?v=VD1b8yAMC7o&list=PLuEZQoW9bRnRBThyGs208ZMrCYBRTvIg2&index=19

--- Orders/Orders.Backend/Program.cs
+++ Orders/Orders.Backend/Program.cs
@@ -15,12 +15,27 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer("name=LocalConnection"));
+builder.Services.AddTransient<SeedDb>();
 builder.Services.AddScoped(typeof(IGenericUnitOfWork<>), typeof(GenericUnitOfWork<>));
 builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
 
 
 
 var app = builder.Build();
+
+SeedData(app);
+//Como en esta clase no puedo inyectar nada porque es la clase inyectora, debo hacer la inyección de dependencias manualmente
+void SeedData(WebApplication app)
+{
+    var scopedFactory = app.Services.GetService<IServiceScopeFactory>();
+
+    using (var scope = scopedFactory!.CreateScope())
+    {
+        var service = scope.ServiceProvider.GetService<SeedDb>();
+        service!.SeedAsync().Wait();
+    }
+}
+
 
 app.UseCors(x => 
         x.AllowAnyMethod()
